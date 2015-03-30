69e1d2a4be30100addbc884a69dae5182f22be14
fsoul <bilinskyivitalii@gmail.com>
2015-03-30 21:43:41 +0300

    verstka recipes

--- application/views/pages/recipes_view.php
+++ application/views/pages/recipes_view.php
@@ -1,9 +1,45 @@
-<div class="container" style="margin-top: 20px; margin-bottom: 10px; ">
-    <div class="row">
-        <div class="col-xs-8 col-xs-offset-2">
-            recipes page
-        </div>
+<div id="recipes_main">
+    <div id="bar">
+        <div id="sort">Все</div>
+        <div id="bordik"><div></div></div>
+        <div id="rec_search">Поиск</div>
     </div>
 
-    <hr>
+    <div id="rec_wrap">
+        <div class="recipe_item">
+            <img class="recipe_item_img" src="<?= base_url('assets/images/site/tmp/3.jpg');?>">
+            <div class="recipe_item_text">
+                <a class="rec_detail" href="#"><p class="recipe_item_title">Ягодный десерт</p></a>
+                <p class="recipe_item_desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac risus ac eros tincidunt mattis ac velificient massa. Vestibulum augue turpis, dignissim tincidunt rutrum sed, lacinia ac mi.</p>
+                <div class="rec_item_cat_wrap">
+                    <div class="recipe_item_cat">
+                        <span class="cat_items">Десерты,</span>
+                        <span class="cat_items">Морепродукты,</span>
+                        <span class="cat_items">Выпечка,</span>
+                        <span class="cat_items">Тесто,</span>
+                        <span class="cat_items">Десерты,</span>
+                    </div>
+                </div>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+        <div class="recipe_item">
+            <img class="recipe_item_img" src="<?= base_url('assets/images/site/tmp/2.jpg');?>">
+            <div class="recipe_item_text">
+                <a class="rec_detail" href="#"><p class="recipe_item_title">Яблочный пирог</p></a>
+                <p class="recipe_item_desc">Lorem ipsum dolor sit amet, consectetur adipiscing espant. Sed ac risus ac eros tincidunt mattis ac vespanficient massa. Vestibulum augue turpis, dignissim tincidunt rutrum sed, lacinia ac mi.</p>
+                <div class="rec_item_cat_wrap">
+                    <div class="recipe_item_cat">
+                        <span class="cat_items">Десерты,</span>
+                        <span class="cat_items">Десерты,</span>
+                        <span class="cat_items">Десерты,</span>
+                        <span class="cat_items">Десерты,</span>
+                        <span class="cat_items">Десерты,</span>
+                    </div>
+                </div>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+    </div
+
 </div>
